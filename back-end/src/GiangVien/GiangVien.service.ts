9962ee7c5c5f5cb690113dac4133856e43202087
pmhanh <150933678+pmhanh@users.noreply.github.com>
2025-04-15 22:16:04 +0700

    Update GiangVien.service.ts

--- back-end/src/GiangVien/GiangVien.service.ts
+++ back-end/src/GiangVien/GiangVien.service.ts
@@ -241,8 +241,8 @@ export class GiangVienService {
   }
 
   async getCourses(id: string): Promise<KhoaHocDocument[]> {
-    const giangVien = await this.giangVienModel.findOne({ MaGV: id }).exec();
-
+    // const giangVien = await this.giangVienModel.findOne({ MaGV: id }).exec();
+    const giangVien = await this.giangVienModel.findById(id).exec();
     if (!giangVien) {
       throw new NotFoundException('Không tìm thấy giảng viên.');
     }
