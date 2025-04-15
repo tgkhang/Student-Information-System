b7b4e5bbd30d186818652066740454d79efc1cdd
huytienlam <huytienlam@gmail.com>
2025-04-15 16:10:39 +0700

    Revert "sửa form forgot password từ email thành username"
    This reverts commit 5f52bebe51af027219e1a7b5f9385073b8b9f347.

--- frontend/src/pages/authentication/ForgotPassword.jsx
+++ frontend/src/pages/authentication/ForgotPassword.jsx
@@ -170,24 +170,24 @@ export default function ForgotPassword() {
                 </Typography>
               </Box>
               <Typography sx={guestLoginSection}>
-                Please type your username in the box below and we will send you a
-                recovery link to your linked email.
+                Please type your email in the box below and we will send you a
+                recovery link.
               </Typography>
             </Box>
 
             <form onSubmit={handleSubmit(onSubmit)} style={guestContainerLoginSubsection}>
               <TextField
-                placeholder="Enter your username"
-                {...register("username")}
+                placeholder="Enter your email"
+                {...register("email")}
                 fullWidth
                 variant="outlined"
                 size="small"
                 InputProps={{
                   sx: { borderRadius: "2em", fontSize: "0.8rem" },
                 }}
-                label="Username"
-                error={!!errors.username}
-                helperText={errors.username?.message}
+                label="Email"
+                error={!!errors.email} // Check if there is an error with the email
+                helperText={errors.email?.message} // Show error message if any
                 InputLabelProps={{
                   sx: {
                     fontSize: "0.8rem",
