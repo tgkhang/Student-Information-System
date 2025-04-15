1832e99150bf8bf216972fcde3e1f4adfbe7a59f
huytienlam <huytienlam@gmail.com>
2025-04-15 16:10:41 +0700

    Revert "Change forgot password and login"
    This reverts commit 59d84c224793b719ccaafd2ec671253f5068612c.

--- frontend/src/pages/authentication/Login.jsx
+++ frontend/src/pages/authentication/Login.jsx
@@ -230,7 +230,6 @@ export default function Login() {
                 />
 
                 <TextField
-                  type="password"
                   placeholder="Enter your password"
                   {...register("password")}
                   fullWidth
