519589867b120eae4700778b8f34bdc7d39e4d45
Nhân Nguyễn Trọng <nhannguyentrong355@gmail.com>
2025-04-24 14:52:52 +0700

    fix bug

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -185,8 +185,6 @@ export const studentCancelRegister= (data,username) =>
 
 export const getListCourseRegister = (studentId)=> 
    axiosInstance.get(`/KhoaHoc/getListCourseRegister/${studentId}`);
-<<<<<<< HEAD
-=======
 
 export const HasRating = (MaKH, MaSV) => {
   return axiosInstance.get(`/KhoaHoc/hasRating/${MaKH}/${MaSV}`);
@@ -195,4 +193,3 @@ export const studentRating = (MaKH, data) => {
   return axiosInstance.post(`/KhoaHoc/rate/${MaKH}`, data);
 }
 
->>>>>>> bd430a98679e37e60dfb4f05bfd9a2593b6fbe89
